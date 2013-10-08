1f35c52a276d4822db9a15b1259d85b2cd569918
ignaciojonas <ignaciojonas@gmail.com>
2013-10-08 14:22:29 -0300

    Roman Numbers

---
+++ KataRomanNumbers/KataRomanNumbers.Tests/ConvertRomanNumbersTestsFixture.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KataRomanNumbers;
+
+namespace KataRomanNumbers.Tests
+{
+    [TestClass]
+    public class ConvertRomanNumbersTestsFixture
+    {
+        [TestMethod]
+        public void ConverToRoman_Parameter1_ReturnI()
+        {
+            var conversor = new RomanNumbersConversos();
+            var romanNumber = conversor.Convert(1);
+            Assert.AreEqual("I", romanNumber);
+        }
+
+        [TestMethod]
+        public void ConverToRoman_Parameter10_ReturnX()
+        {
+            var conversor = new RomanNumbersConversos();
+            var romanNumber = conversor.Convert(10);
+            Assert.AreEqual("X", romanNumber);
+        }
+
+        [TestMethod]
+        public void ConverToRoman_Parameter7_ReturnVII()
+        {
+            var conversor = new RomanNumbersConversos();
+            var romanNumber = conversor.Convert(7);
+            Assert.AreEqual("VII", romanNumber);
+        }
+
+        [TestMethod]
+        public void ConverToRoman_Parameter5_ReturnV()
+        {
+            var conversor = new RomanNumbersConversos();
+            var romanNumber = conversor.Convert(5);
+            Assert.AreEqual("V", romanNumber);
+        }
+
+        [TestMethod]
+        public void ConverToRoman_Parameter11_ReturnXI()
+        {
+            var conversor = new RomanNumbersConversos();
+            var romanNumber = conversor.Convert(11);
+            Assert.AreEqual("XI", romanNumber);
+        }
+
+        [TestMethod]
+        public void ConverToRoman_Parameter4_ReturnIV()
+        {
+            var conversor = new RomanNumbersConversos();
+            var romanNumber = conversor.Convert(4);
+            Assert.AreEqual("IV", romanNumber);
+        }
+
+        [TestMethod]
+        public void ConverToRoman_Parameter9_ReturnIX()
+        {
+            var conversor = new RomanNumbersConversos();
+            var romanNumber = conversor.Convert(9);
+            Assert.AreEqual("IX", romanNumber);
+        }
+
+        [TestMethod]
+        public void ConverToRoman_Parameter14_ReturnXIV()
+        {
+            var conversor = new RomanNumbersConversos();
+            var romanNumber = conversor.Convert(14);
+            Assert.AreEqual("XIV", romanNumber);
+        }
+        
+        [TestMethod]
+        public void ConverToRoman_Parameter49_ReturnXLIX()
+        {
+            var conversor = new RomanNumbersConversos();
+            var romanNumber = conversor.Convert(49);
+            Assert.AreEqual("XLIX", romanNumber);
+        }
+
+        [TestMethod]
+        public void ConverToRoman_Parameter70_ReturnLXX()
+        {
+            var conversor = new RomanNumbersConversos();
+            var romanNumber = conversor.Convert(70);
+            Assert.AreEqual("LXX", romanNumber);
+        }
+
+        [TestMethod]
+        public void ConverToRoman_Parameter79_ReturnLXXIX()
+        {
+            var conversor = new RomanNumbersConversos();
+            var romanNumber = conversor.Convert(79);
+            Assert.AreEqual("LXXIX", romanNumber);
+        }
+    }
+}
---
+++ KataRomanNumbers/KataRomanNumbers/RomanNumbersConversos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KataRomanNumbers
+{
+    public class RomanNumbersConversos
+    {
+        public string Convert(int p)
+        {
+            string ret = string.Empty;
+            GetSimbol(ref p, ref ret, 50, "L");
+            GetSimbol(ref p, ref ret, 40, "XL");
+            GetSimbol(ref p, ref ret,10,"X");
+            GetSimbol(ref p, ref ret, 9, "IX");
+            GetSimbol(ref p, ref ret, 5, "V");
+            GetSimbol(ref p, ref ret, 4, "IV");
+            GetSimbol(ref p, ref ret, 1, "I");
+
+            return ret;
+        }
+
+        private static void GetSimbol(ref int p, ref string ret, int mod, string simbol)
+        {
+            if (p > 0)
+            {
+                var mod10 = p / mod;
+                while (mod10 > 0)
+                {
+                    ret += simbol;
+                    mod10--;
+                    p = p - mod;
+                }
+            }
+        }
+    }
+}
